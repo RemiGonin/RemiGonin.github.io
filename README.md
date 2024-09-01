ab768193be9293f41818552965276a3161cbd346
Remi Gonin <remi.gonin@datadoghq.com>
2024-09-01 15:07:58 +0200

    go simpler

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # personnalWebsite
-
-
-Site personnel : présentation, CV, projets, et expérimentations de developpement web.
-
-EN CONSTRUCTION
--- scripts/Cell.js
+++
@@ -1,9 +0,0 @@
-// Cell in the grid. Contains coordinates, and walkbality
-
-function Cell(x, y, obstructed) {
-    this.x = x; //int
-    this.y = y; //int
-    this.obstructed = obstructed; //bool : false if cell is accessible
-    this.start = false;
-    this.goal = false;
-}
